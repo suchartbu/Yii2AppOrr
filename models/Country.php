f5e69052862d0a1eef818bfe7b0772185cad57eb
suchart bunhachirat <suchartbu@gmail.com>
2019-11-12 13:10:21 +0700

    use Gii

--- models/Country.php
+++ models/Country.php
@@ -1,12 +1,49 @@
 <?php
+
 namespace app\models;
-use yii\db\ActiveRecord;
+
+use Yii;
 
 /**
- * Description of Country
+ * This is the model class for table "country".
  *
- * @author it
+ * @property string $code
+ * @property string $name
+ * @property int $population
  */
-class Country extends ActiveRecord {
-    //put your code here
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
